6ba28b4f8d01468a16d2fd7dbc40986267bd8146
pretty128guydev <night.fury.1208.0508@gmail.com>
2025-04-21 16:09:48 -0400

    fix issue

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,22 @@ import { defineConfig } from 'vite'; // eslint-disable-line
 import react from '@vitejs/plugin-react'; // eslint-disable-line
 
 export default defineConfig({
-  base: '/', // Or '/repo-name/' if using GitHub Pages
+  base: '/', // or '/your-repo-name/' for GitHub Pages
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-  },
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
-      external: ['react', 'react-dom'],
       output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
         manualChunks(id) {
           if (id.includes('node_modules')) {
             if (id.includes('react')) return 'vendor-react';
             return 'vendor';
           }
-          return null;
         },
       },
     },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
   },
 });
