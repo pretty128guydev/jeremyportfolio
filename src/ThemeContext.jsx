616edda9b48b9d0f5c25b7324b0367e5579d3985
pretty128guydev <night.fury.1208.0508@gmail.com>
2025-04-22 11:34:56 -0400

    fix

--- src/ThemeContext.jsx
+++ src/ThemeContext.jsx
@@ -1,7 +1,7 @@
-import React, { useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const ThemeContext = React.createContext();
+const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState('dark');
