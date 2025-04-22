c0b32c554ef3a1ba311f68894c36bf7fd80a1543
pretty128guydev <night.fury.1208.0508@gmail.com>
2025-04-22 11:47:52 -0400

    fix isseu

--- src/ThemeContext.jsx
+++ src/ThemeContext.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState } from 'react';
+import * as React from 'react';
+import { createContext, useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const ThemeContext = createContext();
